e167f99378ce7a16d8919d484bed464d8f1f163d
daihougi <hima71f@gmail.com>
2021-12-16 12:04:31 +0900

    add new file

--- web/index.php
+++ web/index.php
@@ -199,7 +199,7 @@
                       document.getElementById("bn").disabled = true;
                   }
               }
-              var myfunc = function (){              
+              function checkdate(){              
                   var todayObj = new Date();
                   var today   = todayObj.getTime();
                   var endObj   = new Date('2019-09-30T23:59:59');  // 終了日の指定
