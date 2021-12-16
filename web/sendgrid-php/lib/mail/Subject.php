2adb6ec2a22c6e7ff564340cde11c52efba625ec
daihougi <hima71f@gmail.com>
2021-12-16 09:55:56 +0900

    add new file

--- web/sendgrid-php/lib/mail/Subject.php
+++ web/sendgrid-php/lib/mail/Subject.php
@@ -60,5 +60,9 @@ class Subject implements \JsonSerializable
      *
      * @return string
      */
-    
+    #[\ReturnTypeWillChange]
+    public function jsonSerialize()
+    {
+        return $this->getSubject();
+    }
 }
