ae0935140fcbac0d61d1aaf69fa5e216a64678a6
daihougi <hima71f@gmail.com>
2021-12-16 12:24:00 +0900

    add new file

--- web/index.php
+++ web/index.php
@@ -62,7 +62,7 @@
                             </label>
                             <label class="contact-skill">
                                 <input type="radio" name="keitai" value="会場参加" <?php if( !empty($_POST['keitai']) && $_POST['keitai'] === "会場参加" ){ echo 'checked'; } ?>/>
-                                <span class="contact-skill-txt">id='edit_area'会場参加　会員限定先着20名（締切3月3日)</span>
+                                <span class="contact-skill-txt"> id='edit_area'会場参加　会員限定先着20名（締切3月3日)</span>
                             </label>                        
                         </td>
                     </tr>
@@ -199,7 +199,7 @@
                       document.getElementById("bn").disabled = true;
                   }
               }
-              function checkdate(){              
+              function checkdate(input){              
                   var todayObj = new Date();
                   var today   = todayObj.getTime();
                   var endObj   = new Date('2019-09-30T23:59:59');  // 終了日の指定
