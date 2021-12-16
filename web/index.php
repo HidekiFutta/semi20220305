a8aa9df87d56b462cc8e5dbcf11e99694088e5f6
daihougi <hima71f@gmail.com>
2021-12-16 14:15:58 +0900

    add new file

--- web/index.php
+++ web/index.php
@@ -62,7 +62,7 @@
                             </label>
                             <label class="contact-skill">
                                 <input type="radio" name="keitai" value="会場参加" <?php if( !empty($_POST['keitai']) && $_POST['keitai'] === "会場参加" ){ echo 'checked'; } ?>/>
-                                <span class="contact-skill-txt"> <div id="edit_area">会場参加　会員限定先着20名（締切3月3日)</div></span>
+                                <span class="contact-skill-txt" id="edit_area">会場参加　会員限定先着20名（締切3月3日)</span>
                             </label>                        
                         </td>
                     </tr>
