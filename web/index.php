2d4b4b24681900b1e84b66f1486c40b92ae16c2f
daihougi <hima71f@gmail.com>
2021-12-16 14:12:49 +0900

    add new file

--- web/index.php
+++ web/index.php
@@ -203,13 +203,13 @@
               var today   = todayObj.getTime();
               var endObj   = new Date('2019-09-30T23:59:59');  // 終了日の指定
               var end   = endObj.getTime();
-              var comment = document.getElementById("edit_area");
+              var comment = "";
               if(today <= end){// 有効期限の範囲内
                   comment= "会場参加　会員限定先着20名（締切3月3日)";
               }else{
                   comment= "会場参加は締め切りました（締切3月3日)";
               }
-              comment.innerHTML = "comment";
+              document.getElementById("edit_area").innerHTML = "commentdesu" ;
               
             // -->
             </script>            
