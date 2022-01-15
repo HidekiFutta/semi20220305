ba8d6fda36538057bdca928d63355981a1d869dd
daihougi <hima71f@gmail.com>
2022-01-15 14:06:45 +0900

    tekito-

--- web/index.php
+++ web/index.php
@@ -88,7 +88,7 @@
                         <td class="contact-body">
                             <label class="contact-keitai">
                                 <input type="radio" name="keitai" value="Web参加" checked="checked" <?php if( !empty($_POST['keitai']) && $_POST['keitai'] === "Web参加" ){ echo 'checked'; } ?>>
-                                <span class="contact-skill-txt">Web参加　先着<?php echo $w_teiin?>名（当日まで受付可）</span>
+                                <span class="contact-skill-txt">Web参加  先着<?php echo $w_teiin?>名（当日まで受付可）</span>
                             </label>
                             <label class="contact-skill">
                                 <input type="radio" id="kaijyo" name="keitai" value="会場参加" <?php if( !empty($_POST['keitai']) && $_POST['keitai'] === "会場参加" ){ echo 'checked'; } ?>/>
